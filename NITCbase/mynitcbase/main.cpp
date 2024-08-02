4b582d187b12e268c68ef029c939ef9bf7ea9f42
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2024-08-02 14:29:12 +0530

    used write and read disk opeartion

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -17,9 +17,33 @@ int main(int argc, char *argv[]) {
   std::memcpy(buffer + 20, message, 6);
   Disk::writeBlock(buffer, 7000);
 
-  std::cout<<buffer<<std::endl;
+  unsigned char buffer1[BLOCK_SIZE];
+  Disk::readBlock(buffer1, 7000);
+  unsigned char message2[6];
+  memcpy(message2, buffer1 + 20, 6);
+  std::cout<<message2<<std::endl;
+  // std::cout<<buffer<<std::endl;
+
   // StaticBuffer buffer;
   // OpenRelTable cache;
 
-  return FrontendInterface::handleFrontend(argc, argv);
+  return 0;
+  // FrontendInterface::handleFrontend(argc, argv);
 }
+// int main(int argc, char *argv[]) {
+//   Disk disk_run;
+
+//   unsigned char buffer[BLOCK_SIZE];
+//   Disk::readBlock(buffer, 7000);
+//   char message[] = "hello";
+//   memcpy(buffer + 20, message, 6);
+//   Disk::writeBlock(buffer, 7000);
+
+//   unsigned char buffer2[BLOCK_SIZE];
+//   char message2[6];
+//   Disk::readBlock(buffer2, 7000);
+//   memcpy(message2, buffer2 + 20, 6);
+//   std::cout << message2;
+
+//   return 0;
+// }
