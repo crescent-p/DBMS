91588ae1b7bcd5a10b84cabb26d1a92793bd0d8a
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2024-08-03 19:52:53 +0530

    created a function to load headinfo buffer

--- NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
+++ NITCbase/mynitcbase/Buffer/BlockBuffer.cpp
@@ -2,3 +2,24 @@
 
 #include <cstdlib>
 #include <cstring>
+
+BlockBuffer::BlockBuffer(int blockNum){
+	this.blockNum = blockNum;
+}
+
+RecBuffer::RecBuffer(int blockNum) : BlockBuffer::BlockBuffer(blockNum){}
+
+int BlockBuffer::getHeader(struct HeadInfo *head){
+	unsigned char buffer[BLOCK_SIZE];
+
+	Disk::readBlock(buffer, this.blockNum);
+
+	memcpy(&head->numSlots, buffer + 24, 4);
+	memcpy(&head->numEntries, buffer + 16, 4);
+	memcpy(&head->numAttrs, buffer + 20, 4);
+	memcpy(&head->rblock, buffer + 12, 4);
+	memcpy(&head->lblock, buffer + 8, 4);
+
+	return SUCCESS;
+}
+
