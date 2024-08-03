e08d2d79ebcf1a5a09df8b8b47acbf2c43da4599
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2024-08-03 19:46:43 +0530

    "stage 2 main file edited"

--- NITCbase/mynitcbase/main.cpp
+++ NITCbase/mynitcbase/main.cpp
@@ -8,24 +8,36 @@ int main(int argc, char *argv[]) {
   /* Initialize the Run Copy of Disk */
   Disk disk_run;
 
-  //creating a string
-  unsigned char buffer[BLOCK_SIZE];
-  //reading the information at 7000th block.
-  Disk::readBlock(buffer, 7000);
-  //writing a new message to that block.
-  unsigned char message[] = "hello";
-  std::memcpy(buffer + 20, message, 6);
-  Disk::writeBlock(buffer, 7000);
+  RecBuffer relCatBuffer(RELCAT_BLOCK);
+  RecBuffer attrCatBuffer(ATTRCAT_BLOCK);
 
-  unsigned char buffer1[BLOCK_SIZE];
-  Disk::readBlock(buffer1, 7000);
-  unsigned char message2[6];
-  memcpy(message2, buffer1 + 24, 6);
-  std::cout<<message2<<std::endl;
-  // std::cout<<buffer<<std::endl;
+  HeadInfo relCatHeader;
+  HeadInfo attrCatHeader;
 
-  // StaticBuffer buffer;
-  // OpenRelTable cache;
+  relCatBuffer.getHeader(&relCatHeader);
+  attrCatBuffer.getHeader(&attrCatHeader);
+
+  for(int i = 0; i <= NO_OF_ATTRS_RELCAT_ATTRCAT; i++){
+      
+      Attribute relCatRecord[RELCAT_NO_ATTRS];
+
+      relCatBuffer.getRecord(relCatRecord, i);
+
+      printf("Relation: %s\n", relCatRecord[RELCAT_REL_NAME_INDEX].sVal);
+
+      for(int j = 0; j <= NO_OF_ATTRS_RELCAT_ATTRCAT; j++){
+
+        Attribute attrCatRecord[ATTRCAT_NO_ATTRS];
+
+        attrCatBuffer.getRecord(attrCatRecord, i);
+
+        if(relCatRecord[i].sVal == attrCatRecord[j].sVal){
+          const char *attrType = attrCatRecord[ATTRCAT_ATTR_TYPE_INDEX].nVal == NUMBER ? "NUM" : "STR";
+          printf("  %s: %s\n", attrCatRecord[i].sVal, attrType);
+        }
+      }
+      std::cout<<std::endl;
+  }
 
   return 0;
   // FrontendInterface::handleFrontend(argc, argv);
