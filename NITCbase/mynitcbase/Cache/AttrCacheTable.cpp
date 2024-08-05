bcda84044d55f187627ca9d85b9f03a529cf614a
ubuntu <ubuntu@ubunu2004.linuxvmimagrs.local>
2024-08-05 10:57:00 +0530

    attrCache table fucntions (getRecord and getRelationcache with relID) completed

--- NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
+++ NITCbase/mynitcbase/Cache/AttrCacheTable.cpp
@@ -1,3 +1,41 @@
 #include "AttrCacheTable.h"
 
 #include <cstring>
+
+AttrCacheEntry *attrCache[MAX_OPEN];
+
+int AttrCacheTable::getAttrCatEntry(int relId, int attrOffset, AttrCatEntry* attrCatBuf){
+	if(relId < 0 || relId >= MAX_OPEN){
+		return E_OUTOFBOUND;
+	}
+
+	if(attrCache[relId] == nullptr){
+		return E_RELNOTOPEN;
+	}
+
+	for(AttrCacheEntry* entry = attrCache[relId]; entry != nullptr; entry = entry->next){
+		if(entry->attrCatEntry.offset == attrOffset){
+			/*
+				typedef struct AttrCatEntry {
+				  char relName[ATTR_SIZE];
+				  char attrName[ATTR_SIZE];
+				  int attrType;
+				  bool primaryFlag;
+				  int rootBlock;
+				  int offset;
+
+				} AttrCatEntry;
+			*/
+			strcpy(attrCatBuf->relName, entry->attrCatEntry.relName); 
+			strcpy(attrCatBuf->attrName, entry->attrCatEntry.attrName);
+			attrCatBuf->attrType = entry->attrCatEntry.attrType;
+			attrCatBuf->primaryFlag = entry->attrCatEntry.primaryFlag;
+			attrCatBuf->rootBlock = entry->attrCatEntry.rootBlock;
+			attrCatBuf->offset = entry->attrCatEntry.offset;
+
+			return SUCCESS;
+		}
+	}
+
+	return E_ATTRNOTEXIST;
+}
--- NITCbase/mynitcbase/Cache/RelCacheTable.cpp
+++ NITCbase/mynitcbase/Cache/RelCacheTable.cpp
@@ -1,3 +1,29 @@
 #include "RelCacheTable.h"
 
 #include <cstring>
+
+RelCacheEntry* RelCacheTable::relCache[MAX_OPEN];
+
+int RelCacheTable::getRelCatEntry(int relId, RelCatEntry* relCatBuf){
+	if(relId < 0 || relId >= MAX_OPEN){
+		return E_OUTOFBOUND;
+	}
+
+	if(relCache[relId] == nullptr){
+		return E_RELNOTOPEN;
+	}
+
+	*relCatBuf = relCache[relId]->relCatEntry;
+
+	return SUCCESS;
+}
+
+void RelCacheTable::recordToRelCatEntry(union Attribute record[RELCAT_NO_ATTRS],
+									RelCatEntry* relCatEntry){
+	strcpy(relCatEntry->relName, record[RELCAT_REL_NAME_INDEX].sVal);
+	relCatEntry->numAttrs = record[RELCAT_NO_ATTRIBUTES_INDEX].nVal;
+	relCatEntry->numRecs = record[RELCAT_NO_RECORDS_INDEX].nVal;
+	relCatEntry->firstBlk = record[RELCAT_FIRST_BLOCK_INDEX].nVal;
+	relCatEntry->lastBlk = record[RELCAT_LAST_BLOCK_INDEX].nVal;
+	relCatEntry->numSlotsPerBlk = record[RELCAT_NO_SLOTS_PER_BLOCK_INDEX].nVal;
+}
